dd8c49db006cb118d9b7aa581e8eb2b0d1d68292
AMK <amkguo@gmail.com>
2016-12-24 23:29:52 -0800

    enter and exit target folder

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 )
 
 var portNo = flag.Int("port", 8088, "The HTTP Port number to listen")
-var dir = flag.String("dir", ".", "The directory to serve")
+var rootDir = flag.String("dir", ".", "The directory to serve")
 
 func main() {
 	flag.Usage = func() {
@@ -18,8 +20,44 @@ func main() {
 	}
 	flag.Parse()
 
-	port := strconv.Itoa(*portNo)
+	// current folder
+	var oldDir = ""
+	var err error
+	if oldDir, err = os.Getwd(); err != nil {
+		log.Fatalf("Can't get current directory: %v", err)
+	}
 
-	fmt.Println("Serve folder " + *dir + " at http://localhost:" + port)
-	http.ListenAndServe(":"+port, http.FileServer(http.Dir(*dir)))
+	// serve the folder "dir"
+	if err = os.Chdir(*rootDir); err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("folder does not exist: %v", err)
+		} else {
+			log.Fatalf("change directory failed: %v", err)
+		}
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, os.Kill)
+
+	port := strconv.Itoa(*portNo)
+	go serveDir(port, *rootDir)
+
+	// wait for Ctrl + C
+	<-ch
+
+	// return to the previous folder
+	if err = os.Chdir(oldDir); err != nil {
+		log.Fatalf("Can't return to %v", oldDir)
+	}
+
+	fmt.Println("Done.")
+}
+
+func serveDir(port string, dir string) {
+	fmt.Printf("Serve folder %q at http://localhost:%s ...\n", dir, port)
+	err := http.ListenAndServe(":"+port, http.FileServer(http.Dir(dir)))
+	if err != nil {
+		log.Fatal("failed to serve")
+	}
 }
